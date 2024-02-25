c5c09f25d090cd2fed3a4470f7324e31b7de2bd5
Ajoke93 <111288233+Ajoke93@users.noreply.github.com>
2024-02-25 16:32:49 -0330

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'ec2-user',
+    DB_HOST : 'database-1.c7gyq2ygkamr.ca-central-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'Sigmatek',
     DB_DATABASE : 'database-1'
